eeb279e52b746206f4507ffea7e6441163f71ec7
DymekGD <mateuszdym@Interia.pl>
2023-06-14 17:02:56 +0200

    fix: RWD

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,8 @@
 const menuText = document.getElementsByClassName(`menu__text`)[0];
 const menu = document.querySelector(`nav ul`);
+const socials = document.querySelector(`aside`);
 
 menuText.addEventListener(`click`, () => {
     (menu.classList.contains(`--visible`)) ? menu.classList.remove(`--visible`) : menu.classList.add(`--visible`);
+    (socials.classList.contains(`--visible`)) ? socials.classList.remove(`--visible`) : socials.classList.add(`--visible`);
 });
